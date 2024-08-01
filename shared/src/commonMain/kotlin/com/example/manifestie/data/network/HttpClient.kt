23c4704fd89ba1e41e81f931b596de60fc3b743d
alexandrakomkova <sashakomkova2001@gmail.com>
2024-08-01 10:44:53 -0700

    redo koin structure for modules

--- shared/src/commonMain/kotlin/com/example/manifestie/data/network/HttpClient.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/data/network/HttpClient.kt
@@ -3,14 +3,13 @@ package com.example.manifestie.data.network
 import com.example.manifestie.core.NETWORK_TIME_OUT
 import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.accept
 import io.ktor.client.request.header
