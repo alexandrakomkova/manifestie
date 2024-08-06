aaa8272e0f1dd447696af61bb99f361d3e66a2aa
alexandrakomkova <sashakomkova2001@gmail.com>
2024-08-06 08:45:52 -0700

    add NetworkError description, some try-catches and refactoring to RandomQuoteScreen

--- shared/src/commonMain/kotlin/com/example/manifestie/presentation/RandomQuoteState.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/presentation/RandomQuoteState.kt
@@ -1,7 +1,5 @@
 package com.example.manifestie.presentation
 
-import com.example.manifestie.core.NetworkError
-
 data class RandomQuoteState (
     val isLoading: Boolean = false,
     val quote: String = "",
