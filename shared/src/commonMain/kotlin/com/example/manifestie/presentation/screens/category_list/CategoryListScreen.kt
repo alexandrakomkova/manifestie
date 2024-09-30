39ca95400a3839f7abaee7b0a06d2c7c9b3b3b59
alexandrakomkova <sashakomkova2001@gmail.com>
2024-09-30 04:04:15 -0700

    add state and vm for category list

--- shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/category_list/CategoryListScreen.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/category_list/CategoryListScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.manifestie.domain.model.Category
-import com.example.manifestie.presentation.screens.random_quote.RandomQuoteViewModel
 import org.koin.compose.getKoin
 
 @Composable
