05f04bf4114eb7c77a7d076329965d11172b136b
alexandrakomkova <sashakomkova2001@gmail.com>
2024-10-24 06:18:07 -0700

    add separated repo for quotes + implement it to firestore repo

--- shared/src/commonMain/kotlin/com/example/manifestie/data/repository/FirestoreCategorySharedRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/data/repository/FirestoreCategorySharedRepositoryImpl.kt
@@ -4,14 +4,14 @@ import com.example.manifestie.core.FIRESTORE_CATEGORY_LIST
 import com.example.manifestie.core.FIRESTORE_QUOTE_LIST
 import com.example.manifestie.domain.model.Category
 import com.example.manifestie.domain.model.Quote
-import com.example.manifestie.domain.repository.CategorySharedRepository
-import com.example.manifestie.domain.repository.QuotesRepository
+import com.example.manifestie.domain.repository.CategoryRepository
+import com.example.manifestie.domain.repository.QuoteRepository
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.firestore.firestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class FirestoreCategorySharedRepositoryImpl: CategorySharedRepository, QuotesRepository {
+class FirestoreCategorySharedRepositoryImpl: CategoryRepository, QuoteRepository {
     private val firestore = Firebase.firestore
 
     override fun getCategories() = flow {
