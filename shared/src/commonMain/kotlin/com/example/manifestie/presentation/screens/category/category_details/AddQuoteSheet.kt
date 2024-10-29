52fe7c975df918b3b8966a19f20189d7fae5b07c
alexandrakomkova <sashakomkova2001@gmail.com>
2024-10-29 02:46:23 -0700

    add sheet for adding quotes

---
+++ shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/category/category_details/AddQuoteSheet.kt
@@ -0,0 +1,62 @@
+package com.example.manifestie.presentation.screens.category.category_details
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.manifestie.presentation.screens.category.AddQuoteEvent
+import com.example.manifestie.presentation.screens.category.AddQuoteSheetState
+import com.example.manifestie.presentation.screens.category.CategorySharedState
+import com.example.manifestie.presentation.screens.components.CustomTextField
+
+@Composable
+fun AddQuoteSheetState(
+    modifier: Modifier = Modifier,
+    addQuoteState: AddQuoteSheetState,
+    state: CategorySharedState,
+    onEvent: (AddQuoteEvent) -> Unit
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .padding(bottom = 60.dp)
+    ) {
+        CustomTextField(
+            labelText = "Quote",
+            title = addQuoteState.quote,
+            setTitle = { onEvent(AddQuoteEvent.OnQuoteContentChanged(it)) } ,
+            maxLines = 5,
+            hasError = addQuoteState.quoteError != null,
+            errorMessage = addQuoteState.quoteError
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(
+            onClick = {
+                onEvent(AddQuoteEvent.SaveQuote)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+            shape = RoundedCornerShape(15.dp),
+        ) {
+            Text(
+                text = if(state.selectedQuote == null ) "Save" else "Update",
+                color = Color.White,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
