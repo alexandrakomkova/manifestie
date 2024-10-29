777b56909263ee5d33916ab2e1ad247b010ba4dc
alexandrakomkova <sashakomkova2001@gmail.com>
2024-10-29 02:19:48 -0700

    add states and events for adding quote and for category details screen

--- shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/category/CategorySharedState.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/category/CategorySharedState.kt
@@ -8,13 +8,16 @@ data class CategorySharedState(
     val categories: List<Category> = emptyList(),
     val quotes: List<Quote> = emptyList(),
     val error: String? = null,
-    val selectedCategoryForQuotes: Category? = null
+    val selectedCategoryForQuotes: Category? = null,
+    val selectedQuote: Quote? = null
 )
 
-sealed interface AddCategoryEvent {
+sealed interface CategoryListEvent
+
+sealed interface AddCategoryEvent: CategoryListEvent {
     data object OnAddCategoryClick: AddCategoryEvent
     data class OnCategoryTitleChanged(val title: String): AddCategoryEvent
-    data object OnCategoryDialogDismiss: AddCategoryEvent
+    data object OnCategorySheetDismiss: AddCategoryEvent
 
     data object DeleteCategory : AddCategoryEvent
     data class SelectCategory(val category: Category): AddCategoryEvent
@@ -22,9 +25,30 @@ sealed interface AddCategoryEvent {
     data object SaveCategory: AddCategoryEvent
 }
 
-data class AddCategoryState (
+data class AddCategorySheetState (
     val title: String = "",
     val titleError: String? = null,
-    val dialogOpen: Boolean = false,
+    val sheetOpen: Boolean = false,
     val selectedCategory: Category? = null
 )
+
+sealed interface CategoryDetailEvent {
+    data object DeleteQuoteFromCategory: CategoryDetailEvent
+    data class SelectCategory(val category: Category): CategoryDetailEvent
+    data class SelectQuote(val quote: Quote): CategoryDetailEvent
+}
+
+data class AddQuoteSheetState (
+    val quote: String = "",
+    val quoteError: String? = null,
+    val sheetOpen: Boolean = false,
+    // val selectedCategory: Category? = null
+)
+
+sealed interface AddQuoteEvent: CategoryDetailEvent {
+    data object OnAddQuoteClick: AddQuoteEvent
+    data class OnQuoteContentChanged(val quote: String): AddQuoteEvent
+    data object OnQuoteSheetDismiss: AddQuoteEvent
+
+    data object SaveQuote: AddQuoteEvent
+}
