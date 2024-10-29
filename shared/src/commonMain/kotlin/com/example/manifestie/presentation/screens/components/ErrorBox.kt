2f32d01af1c45210f0028be23f4e94f849922923
alexandrakomkova <sashakomkova2001@gmail.com>
2024-10-29 02:45:51 -0700

    move common compose functions to common folder

--- shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/components/ErrorBox.kt
+++ shared/src/commonMain/kotlin/com/example/manifestie/presentation/screens/components/ErrorBox.kt
@@ -1,4 +1,4 @@
-package com.example.manifestie.core
+package com.example.manifestie.presentation.screens.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
