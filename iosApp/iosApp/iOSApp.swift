b9df1105dc962f22aebf184eec8eaa0a13534505
alexandrakomkova <sashakomkova2001@gmail.com>
2024-11-13 23:59:15 -0800

    add notification to ios part

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,11 +1,53 @@
 import SwiftUI
 import shared
 import FirebaseCore
+import UserNotifications
+
+
+class NotificationManager {
+    static func requestNotificationPermissions() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("Notification permission granted.")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    static func scheduleDailyNotification(title: String, message: String, hour: Int, minute: Int) {
+            let content = UNMutableNotificationContent()
+            content.title = title
+            content.body = message
+            content.sound = .default
+
+            var dateComponents = DateComponents()
+            dateComponents.hour = hour
+            dateComponents.minute = minute
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+            let request = UNNotificationRequest(identifier: "dailyNotification", content: content, trigger: trigger)
+
+            UNUserNotificationCenter.current().add(request) { error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+}
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
+      NotificationManager.requestNotificationPermissions()
+      NotificationManager.scheduleDailyNotification(
+        title: "Daily Reminder",
+        message: "Don't forget to check your tasks!",
+        hour: 10,
+        minute: 0
+      )
+
 
     return true
   }
